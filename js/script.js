e6e892f1ed9613a4624570cf2c4799449adaf969
darwinramadhan <darwinramadhan12@gmail.com>
2021-10-09 15:16:35 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var countDate = new Date('Jan 01 2022 00:00:00').getTime();
+
+function newYear(){
+  var now = new Date().getTime();
+  gap = countDate -now;
+
+  var detik = 1000;
+  var menit = detik * 60;
+  var jam = menit * 60;
+  var hari = jam * 24;
+
+  var h = Math.floor(gap / (hari));
+  var j = Math.floor((gap % (hari)) / (jam));
+  var m = Math.floor((gap % (jam)) / (menit));
+  var d = Math.floor((gap % (menit)) / (detik));
+
+  document.getElementById('hari').innerText = h;
+  document.getElementById('jam').innerText = j;
+  document.getElementById('menit').innerText = m;
+  document.getElementById('detik').innerText = d;
+}
+
+setInterval( function() {
+  newYear();
+}, 1000);
+
+
+var myAudio = document.getElementById('audio');
+var isPlaying = false;
+function togglePlay() {
+  myAudio.play();
+  terakhir = document.querySelector('#cover');
+  terakhir.classList.add('hide');
+};
